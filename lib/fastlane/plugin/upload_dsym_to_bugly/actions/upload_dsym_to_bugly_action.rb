73862525132edc2a42333962bf286c4044445422
liubo <liubo004@126.com>
2018-11-12 16:56:16 +0800

    [IMP] add code

--- lib/fastlane/plugin/upload_dsym_to_bugly/actions/upload_dsym_to_bugly_action.rb
+++ lib/fastlane/plugin/upload_dsym_to_bugly/actions/upload_dsym_to_bugly_action.rb
@@ -5,7 +5,42 @@ module Fastlane
   module Actions
     class UploadDsymToBuglyAction < Action
       def self.run(params)
-        UI.message("The upload_dsym_to_bugly plugin is working!")
+        require 'json'
+        # fastlane will take care of reading in the parameter and fetching the environment variable:
+        UI.message "file path: #{params[:file_path]}"
+
+        json_file = 'upload_dsym_to_bugly_result.json'
+
+        begin
+          api_version = ''
+          if !params[:api_version].nil? && params[:api_version] > 0
+            api_version = " --form \"api_version=#{params[:api_version]}\" "
+            UI.message "api_version: #{api_version}"
+          end
+        rescue Exception => e
+          UI.message "error at checking api version, caused by #{e}"
+        end
+        
+        begin
+          channel = ''
+          if !params[:channel].nil? && !params[:channel].empty?
+            channel = " --form \"channel=#{params[:channel]}\" "
+            UI.message "channel: #{channel}"
+          end
+        rescue Exception => e
+          UI.message "error at checking api version, caused by #{e}"
+        end
+
+        cmd = "curl -k \"https://api.bugly.qq.com/openapi/file/upload/symbol?app_key=#{params[:app_key]}&app_id=#{params[:app_id]}\"" +  api_version + " --form \"app_id=#{params[:app_id]}\"" + " --form \"app_key=#{params[:app_key]}\""  + " --form \"symbolType=#{params[:symbol_type]}\"" + " --form \"bundleId=#{params[:bundle_id]}\"" + " --form \"productVersion=#{params[:product_version]}\"" + channel + " --form \"fileName=#{params[:file_name]}\"" + " --form \"file=@#{params[:file_path]}\"" + " -o " + json_file
+        sh(cmd)
+        obj = JSON.parse(File.read(json_file))
+        ret = obj['rtcode']
+        if ret == 0 
+          UI.message "upload success"
+        else 
+          UI.message "upload failed, result is #{obj}"
+        end
+        `rm upload_dsym_to_bugly_result.json`
       end
 
       def self.description
@@ -27,11 +62,6 @@ module Fastlane
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "UPLOAD_DSYM_TO_BUGLY_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
           FastlaneCore::ConfigItem.new(key: :api_version,
                                   env_name: "FL_UPLOAD_DSYM_TO_BUGLY_API_VERSION",
                                description: "api version for UploadDsymToBuglyAction",
--- lib/fastlane/plugin/upload_dsym_to_bugly/version.rb
+++ lib/fastlane/plugin/upload_dsym_to_bugly/version.rb
@@ -1,5 +1,5 @@
 module Fastlane
   module UploadDsymToBugly
-    VERSION = "0.2.0"
+    VERSION = "1.0.0"
   end
 end
